119acfa72f7a07842676ac8d51b5b01a21ec1736
wongpohling <wongpl-wm17@student.tarc.edu.my>
2019-11-11 11:06:43 +0800

    Initial

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener{
+            calculateBMI()
+        }
+
+        buttonReset.setOnClickListener{
+            editTextWeight.setText("")
+            editTextHeight.setText("")
+            textViewBMI.setText("")
+            imageViewProfilePicture.setImageResource(R.drawable.empty)
+        }
+
+    }
+
+    private fun calculateBMI() {
+        if(editTextWeight.text.isEmpty()){
+            editTextWeight.setError(getString(R.string.input_error))
+            return
+        }
+        if(editTextHeight.text.isEmpty()){
+            editTextHeight.setError(getString(R.string.input_error))
+            return
+        }
+
+        val weight = editTextWeight.text.toString().toFloat()
+        val height = editTextHeight.text.toString().toFloat()
+        val bmi = weight / (height/100).pow(2)
+
+        if(bmi < 18.5){
+            textViewBMI.text = String.format("%s %.2f (%s)", getString(R.string.bmi), bmi, getString(R.string.under))
+            imageViewProfilePicture.setImageResource(R.drawable.under)
+        }else if(bmi in 18.5 .. 24.9){
+            textViewBMI.text = String.format("%s %.2f (%s)", getString(R.string.bmi), bmi, getString(R.string.normal))
+            imageViewProfilePicture.setImageResource(R.drawable.normal)
+        }else{
+            textViewBMI.text = String.format("%s %.2f (%s)", getString(R.string.bmi), bmi, getString(R.string.over))
+            imageViewProfilePicture.setImageResource(R.drawable.over)
+        }
+
+    }
+
+
+}
